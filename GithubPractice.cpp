d72bb1e3f6f49b5fa1de64b14e4ba320c9ebbd99
david <david@DESKTOP-C0KD4V9>
2023-01-19 11:55:06 -0600

    Add project files.

---
+++ GithubPractice.cpp
@@ -0,0 +1,21 @@
+/*Name: David Dillon
+* Date: 1/18/23
+* Program title: GithubPractice
+* Program Description: simple code used for practice committing to a repository
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    string fish;
+    cout << "insert a word to post to the screen" << endl;
+    cin >> fish;
+
+    cout << "\n" << fish << endl;
+}
+
+
